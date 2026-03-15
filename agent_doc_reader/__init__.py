77c65fd6ca0c4e8ac992907ce1d97b8966cb8023
Duy <kmktag904@gmail.com>
2026-03-14 20:14:56 -0400

    add document reference for downstream agent

--- agent_doc_reader/__init__.py
+++ agent_doc_reader/__init__.py
@@ -1,8 +1,10 @@
 from .main import audit_document, query_contracts
-from .services import AuditResult
+from .services import AuditResult, AgentDocReaderService, DocumentReference
 
 __all__ = [
     "query_contracts",
     "audit_document",
     "AuditResult",
+    "AgentDocReaderService",
+    "DocumentReference",
 ]
